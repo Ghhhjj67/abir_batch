1ca0db811840b10b616e247a63de2a9d387285dd
yogeshmirro <101546464+yogeshmirro@users.noreply.github.com>
2023-01-08 22:24:50 +0530

    Update not_big.py

--- bot/core/handlers/not_big.py
+++ bot/core/handlers/not_big.py
@@ -22,7 +22,7 @@ async def handle_not_big(
     _db_caption = await db.get_caption(m.from_user.id if hasattr(m.from_user,"id") else Config.OWNER_ID)
     apply_caption = await db.get_apply_caption(m.from_user.id if hasattr(m.from_user,"id") else Config.OWNER_ID)
     if (not _db_caption) and (apply_caption is True):
-        caption = re.sub(f"{Config.REMOVE_CAPTION}","",m.reply_to_message.caption.markdown) \
+        caption = re.sub(f"{Config.REMOVE_CAPTION}","",m.reply_to_message.caption.markdown).replace("*","") \
             if m.reply_to_message.caption \
             else ""
     elif _db_caption and (apply_caption is True):
